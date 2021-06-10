647e0841e4951fe872209721b1cbf570e45e954e
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-06-10 20:18:59 +0800

    template completed

--- js/all.js
+++ js/all.js
@@ -1,4 +1,18 @@
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+});
+
+VeeValidateI18n.loadLocaleFromURL('../zh_TW.json');
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
+
 import productModal from './productModal.js';
+
 
 const api_path =  'jacky';
 const base_url =  'https://vue3-course-api.hexschool.io/';
@@ -30,10 +44,14 @@ const app = Vue.createApp({
 
     },
     mounted() {
-        // this.$refs.userProductModal.openModal();
+        this.$refs.userProductModal.openModal();
     },
 });
 
 app.component('userProductModal', productModal);
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
 app.mount("#app");
